98d8ce5bb0eaa1da6c65aa5460dc9ddbf5330c67
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-02-07 23:16:13 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,14 @@
 class Backer
 
-  attr_accessor :name
+  attr_accessor :name, :backed_projects
 
     def initialize(name)
       @name = name
+      @backed_projects = []
     end
 
-  end 
+    def back_project(project)
+      @backed_projects << project
+    end 
+
+  end
