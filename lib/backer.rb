518e46349bc865fb0ee020da7708f119dec1cd94
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-02-07 23:24:39 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,7 +9,7 @@ class Backer
 
     def back_project(project)
       @backed_projects << project
-      @backed_projects.backers == self unless @backed_projects.backers == self 
+      @backed_projects.backers == self 
     end
 
   end
